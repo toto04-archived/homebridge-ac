9380dce3352e02e98403d44574fd9ca23a2d896d
Tommaso Morganti <tommaso.morganti01@gmail.com>
2019-06-19 00:03:35 +0200

    cambiato a heatercooler

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var http = require('http');
 var Accessory, Service, Characteristic, UUIDGen;
 
-testObj = {
-  on: false,
-  hc: 0,
-  temp: 24.5
-}
-
 module.exports = function (homebridge) {
   console.log("homebridge API version: " + homebridge.version);
 
@@ -26,108 +20,78 @@ class ACUnit {
     this.config = config
     this.name = config.name
 
-    this.fan = new Service.Fan(this.name, 'fan')
-    this.fan.getCharacteristic(Characteristic.On)
+    this.on = 0
+    this.hc = 0
+    this.temp = 24
+
+    this.service = new Service.HeaterCooler(this.name, 'Air conditioner')
+    this.service.getCharacteristic(Characteristic.Active)
       .on('set', (value, callback) => {
-        console.log('Fan set to:', value)
-        testObj.on = value
-        callback();
+        this.on = value
+        if (this.on) {
+          console.log('AC activated')
+        } else {
+          console.log('AC turned off')
+        }
+        callback()
       })
       .on('get', (callback) => {
-        console.log('Fan is', testObj.value)
-        callback(null, testObj.value)
+        callback(null, this.on)
       })
 
-
-    this.thermo = new Service.Thermostat(this.name, 'thermostat')
-    this.thermo.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+    this.service.getCharacteristic(Characteristic.CurrentHeaterCoolerState)
       .on('set', (value, callback) => {
         console.log('Current hc state set to:', value)
-        testObj.hc = value
+        this.hc = value
         callback()
       })
       .on('get', (callback) => {
-        console.log('current hc state is', testObj.hc)
-        callback(null, testObj.hc)
+        console.log('Current hc state is', this.hc)
+        callback(null, this.hc)
       })
-      // .setProps({
-      //   maxValue: 2,
-      //   validValues: [0, 2]
-      // })
 
-    this.thermo.getCharacteristic(Characteristic.TargetHeatingCoolingState)
+    this.service.getCharacteristic(Characteristic.TargetHeaterCoolerState)
       .on('set', (value, callback) => {
         console.log('Target hc state set to:', value)
-        testObj.hc = value
+        this.hc = value
         callback()
       })
       .on('get', (callback) => {
-        console.log('Target hc state is', testObj.hc)
-        callback(null, testObj.hc)
+        console.log('Target hc state is', this.hc)
+        callback(null, this.hc)
       })
-      // .setProps({
-      //   maxValue: 2,
-      //   validValues: [0, 2]
-      // })
 
-    this.thermo.getCharacteristic(Characteristic.CurrentTemperature)
+    this.service.getCharacteristic(Characteristic.CurrentTemperature)
       .on('set', (value, callback) => {
         console.log('Current temperature set to:', value)
-        testObj.temp = value
+        this.temp = value
         callback()
       })
       .on('get', (callback) => {
-        console.log('Current temperature is', testObj.temp)
-        callback(null, testObj.temp - 1)
+        console.log('Current temperature is', this.temp)
+        callback(null, this.temp)
       })
 
-    this.thermo.getCharacteristic(Characteristic.TargetTemperature)
+    this.service.addCharacteristic(Characteristic.CoolingThresholdTemperature)
       .on('set', (value, callback) => {
-        console.log('Target temperature set to:', value)
-        testObj.temp = value
-        callback()
+        console.log('cooling thasds is', value)
       })
       .on('get', (callback) => {
-        console.log('Target temperature is', testObj.temp)
-        callback(null, testObj.temp)
+        callback(null, 20)
       })
 
-    this.thermo.getCharacteristic(Characteristic.TemperatureDisplayUnits)
+    this.service.addCharacteristic(Characteristic.HeatingThresholdTemperature)
       .on('set', (value, callback) => {
-        console.log('Disp units set to:', value)
-        callback()
+        console.log('heating thasds is', value)
       })
       .on('get', (callback) => {
-        console.log('Disp units is 0')
-        callback(null, 0)
-      })
-
-    this.fan.addCharacteristic(Characteristic.TargetTemperature)
-      .on('set', (value, callback) => {
-        console.log('Target temperature set to:', value)
-        testObj.temp = value
-        callback()
-      })
-      .on('get', (callback) => {
-        console.log('Target temperature is', testObj.temp)
-        callback(null, testObj.temp)
+        callback(null, 20)
       })
   }
 
   getServices() {
     return [
-      //this.fan,
-      this.thermo
+      this.service
     ]
-  }
-
-  getTargetHeatingCoolingState(callback) {
-    console.log('THC is:', testObj.hc)
-    callback(null, value)
-  }
-
-  setTargetHeatingCoolingState(value, callback) {
-    console.log('THC set to:', testObj.hc)
-    callback(null, value)
   }
 }
